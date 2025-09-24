bac3840897ebdc9db55a0c59596bfa331d40d0a6
Carmine Paolino <carmine@paolino.me>
2025-09-24 16:17:57 +0200

    Run full test suite in latest Ruby and Rails version and upload test coverage from there only.

--- spec/support/simplecov_configuration.rb
+++ spec/support/simplecov_configuration.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/vendor/'
-  add_filter 'acts_as_legacy.rb'
+unless ENV['SKIP_COVERAGE']
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/vendor/'
+    add_filter 'acts_as_legacy.rb'
 
-  enable_coverage :branch
+    enable_coverage :branch
 
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::SimpleFormatter,
-      (SimpleCov::Formatter::Codecov if ENV['CODECOV_TOKEN']),
-      SimpleCov::Formatter::CoberturaFormatter
-    ].compact
-  )
+    formatter SimpleCov::Formatter::MultiFormatter.new(
+      [
+        SimpleCov::Formatter::SimpleFormatter,
+        (SimpleCov::Formatter::Codecov if ENV['CODECOV_TOKEN']),
+        SimpleCov::Formatter::CoberturaFormatter
+      ].compact
+    )
+  end
 end
